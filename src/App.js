f55e56d31b42596feb71e8b7c9ccb05b09ad0e1f
Shadowborn <james.starks023@gmail.com>
2019-09-11 19:37:53 -0600

    Setting up react routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import {BrowserRouter as Router, Route} from "react-router-dom";
+
+import Navbar from "./components/Navbar"
+import ShipsList from "./components/ShipsList"
+import EditShips from "./components/EditShips"
+import CreateShips from "./components/CreateShips"
+import CreateUser from "./components/CreateUser"
 
 function App() {
   return (
-    <div className="container">
-      boo
-    </div>
+    <Router>
+      <Navbar />
+      <Route path="/" exact component={ShipsList} />
+      <Route path="/edit/:id" exact component={EditShips} />
+      <Route path="/create" exact component={CreateShips} />
+      <Route path="/user" exact component={CreateUser} />
+    </Router>
   );
 }
 
